7f853f85b682371f6a9be10aaed3ac85d12e0a31
weishengwx <2234346829@qq.com>
2022-12-04 23:25:39 +0800

    增添swgger配置高

---
+++ src/main/java/com/wx/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.wx.config;
+
+/**
+ * @author 22343
+ * @version 1.0
+ */
+public class SwaggerConfig {
+}
---
+++ src/main/java/com/wx/config/SwaggerConfiguration.java
@@ -0,0 +1,29 @@
+package com.wx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+	
+	
+	@Bean
+	public Docket defaultApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(groupApiInfo()).groupName("默认接口").select().apis(RequestHandlerSelectors.basePackage("com.xiaominfo.knife4j.controller"))
+				//.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+				.paths(PathSelectors.any()).build();
+	}
+	
+	
+	private ApiInfo groupApiInfo() {
+		return new ApiInfoBuilder().title("swagger-bootstrap-ui很棒~~~！！！").description("swagger-bootstrap-ui-demo RESTful APIs").termsOfServiceUrl("http://www.group.com/").contact("group@qq.com").version("1.0").build();
+	}
+}
